98c01eb3dd833e7736674fc7cba6413328dc0203
Noam Rosenthal <noamr@users.noreply.github.com>
2023-12-31 11:42:24 +0000

    Add missing files

---
+++ src/nav.js
@@ -0,0 +1,123 @@
+// @ts-check
+/**
+ * @typedef {import("./types.ts").Config} Config
+ * @typedef {import("./types.ts").NavigationInfo} NavigationInfo
+ * @typedef {import("./types.ts").ExtendedNavigationType} ExtendedNavigationType
+ * */
+
+import { start } from "./transition.js";
+
+/** @param {Config} config */
+export function init(config) {
+    const routes = new Map(Object.entries(config.routes).map(([name, pattern]) => [name, new URLPattern(pattern, location.href)]));
+
+    /** @type {{to: string | null, from: string | null, type: ExtendedNavigationType, class: string | null}[]} */
+    const rules = config.rules.flatMap(nav => {
+        const navs =
+            ("from" in nav || "to" in nav) ? [{from: Reflect.get(nav, "from"), to:Reflect.get(nav, "to")}]:
+            ("with" in nav) ? [{from: nav.with}, {to: nav.with}] :
+            ("between" in nav) ? [
+                {from: nav.between[0], to: nav.between[1]},
+                {from: nav.between[1], to: nav.between[0]}] : [];
+        return navs.map(n => ({from: n.from, to: n.to, type: nav.type || "auto", class: nav.class ?? null}));
+    }).reverse();
+
+    /**
+     * @param {ExtendedNavigationType} expected
+     * @param {NavigationType} actual
+     * @param {number} delta
+     */
+    function matchNavType(expected, actual, delta) {
+        switch (expected) {
+            case "auto":
+                return actual !== "reload";
+            case "back":
+                return actual === "traverse" && delta === -1;
+            case "forward":
+                return actual === "traverse" && delta === 1;
+            default:
+                return actual === expected;
+        }
+    }
+
+    /**
+     *
+     * @param {object} navigation
+     * @param {"push" | "replace" | "traverse" | "reload"} navigation.navigationType
+     * @param {number} navigation.traverseDelta
+     * @param {string} navigation.to
+     * @param {string} navigation.from
+     * @returns {NavigationInfo | null}
+     */
+    function findMatchingNav(navigation) {
+        for (const rule of rules) {
+            if (!matchNavType(rule.type, navigation.navigationType, navigation.traverseDelta))
+                continue;
+
+            /** @type {{[key: string]: string}} */
+            const params = {};
+            /**
+             *
+             * @param {URLPatternResult} result
+             * @returns {{[key: string]: string}}
+             */
+            const applyParams = result => result ?
+                Object.assign(params, result?.pathname?.groups, result?.search?.groups) : {};
+
+            const defaultRoute = new URLPattern("*", location.href);
+            const from = routes.get(rule.from ?? "") ?? defaultRoute;
+            const to = routes.get(rule.to ?? "") ?? defaultRoute;
+            if (applyParams(from.exec(navigation.from)) &&
+                applyParams(to.exec(navigation.to)))
+                return {params, class: rule.class, from: rule.from, to: rule.to};
+        }
+
+        return null;
+    }
+
+    return {
+        findMatchingNav,
+        /**
+         *
+         * @param {NavigateEvent} event
+         * @returns {NavigationInfo | null}
+         */
+        findMatchingNavForNavigateEvent : event =>
+            findMatchingNav({
+                from: window.navigation.currentEntry.url,
+                to: event.destination.url,
+                navigationType: event.navigationType,
+                traverseDelta: event.destination.index - window.navigation.currentEntry.index}),
+        /**
+         *
+         * @param {NavigationInfo} nav
+         * @param {ViewTransition} viewTransition
+         * @param {"old-only" | "new-only" | "both"} phase
+         */
+        startNavigation(nav, viewTransition, phase) {
+            /**
+             *
+             * @param {string} str
+             * @returns
+             */
+            const sub = str =>
+                str.replace(/\$\(([^\)]+)\)/g, (_, t) => nav.params[t] ?? _);
+
+            const captures =
+                Object.fromEntries(Object.entries(config.captures).map(([selector, name]) =>
+                        [sub(selector), sub(name)]));
+
+            return start({
+                viewTransition,
+                classes: {
+                    old: [`route-${nav.from}`],
+                    new: [`route-${nav.to}`],
+                    both: [`from-${nav.from}`, `to-${nav.to}`, ...(nav.class ? [nav.class] : [])]
+                },
+                styles: config.styles,
+                captures
+            }, phase);
+        }
+    };
+}
+
---
+++ src/transition.js
@@ -0,0 +1,126 @@
+// @ts-check
+/// <reference path="global.d.ts" />
+
+/**
+ * @param {import("./types").ViewTransitionParams} transitionParams
+ * @param {"new-only" | "old-only" | "both"} phase
+ * @returns void
+ * */
+export async function start(transitionParams, phase) {
+    /** @type {Set<HTMLElement>} */
+    const elements = new Set();
+    const stylesheet = new CSSStyleSheet();
+
+    const oldClasses = [...(transitionParams.classes?.old || []), "old"];
+    const newClasses = [...(transitionParams.classes?.new || []), "new"];
+
+    if (phase !== "new-only") {
+        applyClasses(oldClasses, true);
+        applyClasses(transitionParams.classes?.both, true);
+        applyCaptures();
+        await transitionParams.viewTransition.updateCallbackDone;
+        applyClasses(oldClasses, false);
+        cleanupCaptures();
+    }
+
+    if (phase === "old-only")
+        return;
+
+    applyClasses(newClasses, true);
+    applyCaptures();
+    document.adoptedStyleSheets.push(stylesheet);
+    await transitionParams.viewTransition.finished;
+    document.adoptedStyleSheets.splice( document.adoptedStyleSheets.indexOf(stylesheet), 1);
+    cleanupCaptures();
+    applyClasses(newClasses, false);
+    applyClasses(transitionParams.classes?.both, false);
+
+    function applyCaptures() {
+        const jobs = Object.entries(transitionParams.captures || {}).map(
+            ([selector, name]) => ({selector, name, root: document.documentElement, params: {}})) ?? [];
+        for (const {selector, name, root, params} of jobs) {
+            /**
+             *
+             * @param {string} str
+             * @param {{[key: string]: string}} args
+             * @returns
+             */
+            const sub = (str, args) =>
+                str.replace(/\$\(([^\)]+)\)/, (original, arg) => args[arg] ?? arg);
+
+            const trimmedSelector = selector.trim();
+            const result = /\[\:([^\]]+)\]/gd.exec(selector);
+            if (result) {
+                for (const element of root.querySelectorAll(trimmedSelector.substr(0, result.index))) {
+                    const newParams = {...params, [result[1]]: element.getAttribute(result[1])};
+                    const next = trimmedSelector.substring(result.indices[0][1]);
+                    jobs.push({selector: next, name, root: /** @type {HTMLElement} */(element), params: newParams})
+                }
+
+                continue;
+            }
+
+            let children = [root];
+            if (selector.length) {
+                try {
+                    children = Array.from(/** @type {NodeListOf<HTMLElement>} */(root.querySelectorAll(selector)));
+                } catch (e) {}
+            }
+
+            for (const element of children) {
+                const nameAfterSubs = sub(name, params);
+                const classes = new Set();
+                const finalName = nameAfterSubs.replace(/\.([^\.]+)/g, (_, cls) => {
+                    classes.add(cls);
+                    return "";
+                });
+
+                element.style.setProperty("view-transition-name", finalName);
+                elements.add(element);
+                applyStyles(finalName, classes);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param {string} name
+     * @param {Set<string>} classes
+     * @returns
+     */
+    function applyStyles(name, classes) {
+        if (phase === "old-only")
+            return;
+        for (let [selector, declaration] of Object.entries(transitionParams.styles || {})) {
+            let found = false;
+            selector = selector.replace(/\(\.([^\)]+)\)/, (t, cls) => {
+                if (!classes.has(cls))
+                    return t;
+                found = true;
+                return `(${name})`;
+            });
+            if (!found)
+                continue;
+
+            const rule = /** @type {CSSStyleRule} */ (stylesheet.cssRules[stylesheet.insertRule(`${selector} {}`)]);
+            for (const prop in declaration)
+                rule.style.setProperty(prop, declaration[/** @type {string | null} */(prop)] ?? null);
+            stylesheet.insertRule(rule.cssText);
+        }
+    }
+
+    function cleanupCaptures() {
+        for (const e of elements)
+            e.style.removeProperty("view-transition-name");
+        elements.clear();
+    }
+
+    /**
+     * @param {string[] | null | undefined} classes
+     * @param {boolean} enable
+     */
+    function applyClasses(classes, enable) {
+        for (const cls of classes || [])
+            document.documentElement.classList.toggle(`vt-${cls}`, enable);
+    }
+}
